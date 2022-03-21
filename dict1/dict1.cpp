6e11c9fc4e0cbb9f8653745dbc7541b7a0e8df82
Sandeep Reddy <msande1996@gmail.com>
2022-03-20 22:59:13 -0400

    startegies strategies

--- dict1/dict1.cpp
+++ dict1/dict1.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 string tryDict1(string cipher, vector<string> candidates) {
-  // if ((float)(cipher.length() - candidates[0].length()) /
-  //         candidates[0].length() <
-  //     0.13) {
-  //   return analyzeNoise(cipher, candidates);
-  // }
+  if ((float)(cipher.length() - candidates[0].length()) /
+          candidates[0].length() <
+      0.15) {
+    return analyzeNoise(cipher, candidates);
+  }
   return analyzeReld(cipher, candidates);
 }
